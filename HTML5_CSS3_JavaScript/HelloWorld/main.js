a03f68710619572c67871a6bf9e31c82867f1c2e
KinhKhaVN <pebin0123@gmail.com>
2024-10-25 07:03:31 +0700

    Add files via upload

---
+++ HTML5_CSS3_JavaScript/HelloWorld/main.js
@@ -0,0 +1,38 @@
+let Person = {
+	name: "Kinh Kha",
+	age: 20,
+	email: "nguyenkinhkha2004@gmail.com",
+}
+
+/*for name in names{
+	console.log(name, ": ", age)
+}*/
+	
+
+const message = "Mèo Méo Meo Mèo Meo";
+conGian = false;
+
+
+function greeting ()
+{
+	alert(message);
+}
+
+function Show()
+{
+	console.log(Person);
+}
+
+let People = [Person, Person, Person];
+
+
+
+
+
+
+
+
+
+
+
+
