bdb77bc2c99ae03ddc80a487771db6f9b809fcbc
KinhKhaVN <pebin0123@gmail.com>
2024-10-25 08:59:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+let Person = {
+	name: "Kinh Kha",
+	age: 20,
+	email: "nguyenkinhkha2004@gmail.com",
+	
+	fun: function(){
+		console.log("Print inside an Object");
+	}
+}
+
+/*for name in names{
+	console.log(name, ": ", age)
+}*/
+	
+
+const message = "Mèo Méo Meo Mèo Meo";
+conGian = false;
+
+
+function greeting ()
+{
+	alert(message);
+}
+
+function Show()
+{
+	console.log(Person);
+}
+
+let People = [Person, Person, Person];
+
+let a = 20;
+let b = 30;
+
+console.log(Person.fun());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
